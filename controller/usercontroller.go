2d1e44f5143acfc5f55250c1464e741ea83ad9bb
RINO Heinrich <rinobeinfo@gmail.com>
2024-06-21 16:00:55 +0200

    divise database package to 3 part, modify usercontroller to use the polymorfic approach

--- controller/usercontroller.go
+++ controller/usercontroller.go
@@ -4,70 +4,45 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/RINOHeinrich/multiserviceauth/database"
 	"github.com/RINOHeinrich/multiserviceauth/helper"
 	"github.com/RINOHeinrich/multiserviceauth/models"
 )
 
-func GetAllUsers(w *http.ResponseWriter, r *http.Request) {
-	cmd := "SELECT * FROM users"
-	var users []models.User
-	stmt, err := database.DatabaseConnection.Prepare(cmd)
-	if err != nil {
-		fmt.Println("Error preparing statement: ", err)
-	}
-	userRows, err := stmt.Query()
-	if err != nil {
-		fmt.Println("Error querying: ", err)
+var db database.Postgres
 
-	}
-	defer userRows.Close()
-	for userRows.Next() {
-		var user models.User
-		if err := userRows.Scan(&user.ID, &user.Username, &user.Password, &user.Email); err != nil {
-			fmt.Println("Error scanning row: ", err)
-			return
-		}
-		users = append(users, user)
-		// Assign the result of append to a variable to avoid the error.
+func GetAllUsers(w *http.ResponseWriter, r *http.Request) {
+
+	users, err := database.FindAll(&db)
+	if err != nil {
+		fmt.Println("Error getting users: ", err)
+		return
 	}
 	json.NewEncoder(*w).Encode(users)
 }
 func GetUser(w *http.ResponseWriter, r *http.Request) {
-	user := models.User{}
 	query := r.URL.Query()
 	id_str := &query["id"][0]
-	id, _ := strconv.Atoi(*id_str)
-	cmd := "SELECT * FROM users WHERE id=$1"
-	stmt, err := database.DatabaseConnection.Prepare(cmd)
-	if err != nil {
-		fmt.Println("Error preparing statement: ", err)
+	if *id_str == "" {
+		http.Error(*w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-	defer stmt.Close()
-	userRow := stmt.QueryRow(id)
-	if err := userRow.Scan(&user.ID, &user.Username, &user.Password, &user.Email); err != nil {
-		fmt.Println("Error scanning row: ", err)
+	user, err := database.Find(&db, *id_str)
+	if err != nil {
+		fmt.Println("Error getting user: ", err)
 		return
 	}
 	json.NewEncoder(*w).Encode(user)
 }
 func InsertUser(w *http.ResponseWriter, r *http.Request) {
-	user := models.User{}
-	json.NewDecoder(r.Body).Decode(&user)
+	user := &models.User{}
+	json.NewDecoder(r.Body).Decode(user)
 	// Handle POST request
 	user.Password = helper.HashPassword(user.Password)
-	stmt, err := database.DatabaseConnection.Prepare("INSERT INTO users (username, password, email) VALUES ($1, $2, $3)")
+	err := database.Insert(&db, user)
 	if err != nil {
-		fmt.Println("Error preparing statement: ", err)
-		return
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(user.Username, user.Password, user.Email)
-	if err != nil {
-		fmt.Println("Error executing statement: ", err)
+		fmt.Println("Error inserting user: ", err)
 		return
 	}
 	fmt.Fprintf(*w, "User inserted: %v\n", user)
@@ -83,15 +58,11 @@ func UpdateUser(w *http.ResponseWriter, r *http.Request) {
 		http.Error(*w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-	id, _ := strconv.Atoi(*id_str)
-	cmd := "UPDATE users SET username=$1, password=$2, email=$3 WHERE id=$4"
-	stmt, err := database.DatabaseConnection.Prepare(cmd)
+	err := database.Update(&db, *id_str, &user)
 	if err != nil {
-		fmt.Println("Error preparing statement: ", err)
+		fmt.Println("Error updating user: ", err)
 		return
 	}
-	defer stmt.Close()
-	stmt.Exec(user.Username, user.Password, user.Email, id)
 	fmt.Fprintf(*w, "PUT request received on id %s", *id_str)
 }
 func DeleteUser(w *http.ResponseWriter, r *http.Request) {
@@ -101,14 +72,10 @@ func DeleteUser(w *http.ResponseWriter, r *http.Request) {
 		http.Error(*w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
-	id, _ := strconv.Atoi(*id_str)
-	cmd := "DELETE FROM users WHERE id=$1"
-	stmt, err := database.DatabaseConnection.Prepare(cmd)
+	err := database.Delete(&db, *id_str)
 	if err != nil {
-		fmt.Println("Error preparing statement: ", err)
+		fmt.Println("Error deleting user: ", err)
 		return
 	}
-	defer stmt.Close()
-	stmt.Exec(id)
 	fmt.Fprintf(*w, "DELETE request received on id %s", *id_str)
 }
